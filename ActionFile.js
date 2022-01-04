dc8188b182fd6800efe9f24f7779138c47ea5344
Rondell Jefferson <rondell804@gmail.com>
2022-01-03 22:08:00 -0600

    Updated Images and New Project Links

--- ActionFile.js
+++ ActionFile.js
@@ -2,6 +2,25 @@ var navbar = document.getElementById("test");
 var hamberger = document.getElementById("menu");
 var sticky = navbar.offsetTop;
 
+//add adnimation on viewpoint of the user
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 75;
+
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
 function scrollAction(){
   if (window.pageYOffset >= sticky) {
    navbar.classList.add("sticky");
@@ -28,3 +47,4 @@ for (var i = 0;i < numberOfButtons;i++){
 
 
 window.addEventListener('scroll', scrollAction);
+window.addEventListener("scroll", reveal);
